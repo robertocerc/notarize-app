6ecc227f992fa4640b5a097e4ca84c9f0e677316
robertorcerc <robertocerc@hotmail.it>
2023-01-05 12:50:11 +0100

    improve ui

--- src/utils/web3modalConfig.js
+++ src/utils/web3modalConfig.js
@@ -3,19 +3,19 @@ import CoinbaseWalletSDK from "@coinbase/wallet-sdk";
 import WalletConnect from "@walletconnect/web3-provider";
 
 const providerOptions = {
-    opera: {
-        package: true,
-    },
-    binancechainwallet: {
-        package: true,
-    },
-    coinbasewallet: {
-        package: CoinbaseWalletSDK,
-        options: {
-            appName: "Web 3 Modal Demo",
-            infuraId: process.env.VUE_APP_INFURA_KEY,
-        },
-    },
+    // opera: {
+    //     package: true,
+    // },
+    // binancechainwallet: {
+    //     package: true,
+    // },
+    // coinbasewallet: {
+    //     package: CoinbaseWalletSDK,
+    //     options: {
+    //         appName: "Web 3 Modal Demo",
+    //         infuraId: process.env.VUE_APP_INFURA_KEY,
+    //     },
+    // },
     walletconnect: {
         package: WalletConnect,
         options: {
